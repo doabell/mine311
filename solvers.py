347549286631dba5232c2c4008f83cee3822b690
doabell <35297086+doabell@users.noreply.github.com>
2022-11-30 01:03:55 -0500

    Write dummy solvers

--- solvers.py
+++ solvers.py
@@ -13,10 +13,23 @@ If there is no safe action, try to click on a safer tile.
 def RandomClicker(board):
     """
     Clicks randomly.
+    Always fails, since it eventually clicks on a mine.
     """
     height = len(board)
     width = len(board[0])
     i = random.randrange(height)
     j = random.randrange(width)
     click = True
+    return click, (i, j)
+
+def RandomFlagger(board):
+    """
+    Flags randomly.
+    Always fails, since it eventually exceeds the mine count.
+    """
+    height = len(board)
+    width = len(board[0])
+    i = random.randrange(height)
+    j = random.randrange(width)
+    click = False
     return click, (i, j)
